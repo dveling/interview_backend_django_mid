5a38190a7c0155aab12edeca7021cf8081ba8f79
David Eling <e_eling@yahoo.com>
2024-05-30 17:49:21 -0400

    Question 2: Creates new api view DeactivateOrderView without tests

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
-from interview.order.serializers import OrderSerializer, OrderTagSerializer
+from interview.order.serializers import OrderSerializer, OrderTagSerializer, DeactivateOrderSerializer
+
 
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+class DeactivateOrderView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = DeactivateOrderSerializer
     
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
